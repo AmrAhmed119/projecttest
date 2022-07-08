4c020c0573f5951eaa1bd82406b4cf6cf4650774
AmrAhmed119 <amreaglegolden@gmail.com>
2022-07-08 14:30:34 +0200

    test class added

---
+++ src/test/java/calTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class calTest {
+
+    @Test
+    void add() {
+        assertEquals(5,cal.add(3,2));
+    }
+}
